b80945d408c4c829f9ce4dd1678946a4dd5c6c9e
Christopher Reichert <creichert07@gmail.com>
2014-05-29 19:02:08 -0500

    Add default initializer functions for model types.

--- src/GUI/StocQt.hs
+++ src/GUI/StocQt.hs
@@ -174,6 +174,37 @@ instance SignalKeyClass DrawVolumeChanged where
 instance SignalKeyClass DrawKLineChanged where
     type SignalParams DrawKLineChanged = IO ()
 
+defaultStockModel :: IO StockModel
+defaultStockModel = do
+    sid <- newMVar "" -- stockId
+    sn  <- newMVar "" -- stockName
+    sdc <- newMVar "d" -- stockDataCycle
+    r   <- newMVar False -- ready
+    sp  <- newMVar 0.0 -- stockPrice
+    m   <- newMVar 0.0 -- stockPriceModified
+    hp  <- newMVar 0.0 -- highestPrice
+    hv  <- newMVar 0.0 -- highestVolume
+    return $ StockModel sid sn sdc r sp m hp hv
+
+defaultStockSettings :: IO StockSettings
+defaultStockSettings = do
+    ct  <- newMVar "year" -- chartType
+    dhp <- newMVar False -- drawHighPrice
+    dlp <- newMVar False -- drawLowPrice
+    dop <- newMVar False -- drawOpenPrice
+    dcp <- newMVar True -- drawClosePrice
+    dv  <- newMVar True -- drawVolume
+    dkl <- newMVar False -- drawKLine
+    cc <- newMVar "#ecc088" -- closeColor
+    hc <- newMVar "#ff0000" -- highColor
+    lc <- newMVar "#00ff00" -- lowColor
+    oc <- newMVar "#0000ff" -- openColor
+    vc <- newMVar "#0000ff" -- volumeColor
+    return $ StockSettings ct dhp dlp dop dcp dv dkl cc hc lc oc vc
+
+defaultStockListModel :: [StockListItem]
+defaultStockListModel = genStockList stocks
+
 getProperty :: (tt -> MVar a) -> ObjRef tt -> IO a
 getProperty gtr = readMVar . gtr . fromObjRef
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Concurrent
 import Graphics.QML
 
 import GUI.StocQt
@@ -9,34 +8,9 @@ import GUI.StocQt
 main :: IO ()
 main = do
 
-    -- StockModel members.
-    sid <- newMVar "" -- stockId
-    sn  <- newMVar "" -- stockName
-    sdc <- newMVar "d" -- stockDataCycle
-    r   <- newMVar False -- ready
-    sp  <- newMVar 0.0 -- stockPrice
-    m   <- newMVar 0.0 -- stockPriceModified
-    hp  <- newMVar 0.0 -- highestPrice
-    hv  <- newMVar 0.0 -- highestVolume
-
-    -- StockSettings
-    ct  <- newMVar "year" -- chartType
-    dhp <- newMVar False -- drawHighPrice
-    dlp <- newMVar False -- drawLowPrice
-    dop <- newMVar False -- drawOpenPrice
-    dcp <- newMVar True -- drawClosePrice
-    dv  <- newMVar True -- drawVolume
-    dkl <- newMVar False -- drawKLine
-    cc <- newMVar "#ecc088" -- closeColor
-    hc <- newMVar "#ff0000" -- highColor
-    lc <- newMVar "#00ff00" -- lowColor
-    oc <- newMVar "#0000ff" -- openColor
-    vc <- newMVar "#0000ff" -- volumeColor
-
+    sm  <- newObjectDC =<< defaultStockModel
+    ss  <- newObjectDC =<< defaultStockSettings
     sis <- mapM newObjectDC $ genStockList stocks
-
-    sm  <- newObjectDC $ StockModel sid sn sdc r sp m hp hv
-    ss  <- newObjectDC $ StockSettings ct dhp dlp dop dcp dv dkl cc hc lc oc vc
     sc  <- newObjectDC $ StocQt sm sis ss
 
     runEngineLoop defaultEngineConfig {
